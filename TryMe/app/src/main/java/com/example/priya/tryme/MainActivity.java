5a04003888068ea27c717a1f613858d8ef312aa2
priyaVora <prvora89@gmail.com>
2018-06-14 16:03:23 -0700

    Finished basic Color Changing android application.

--- TryMe/app/src/main/java/com/example/priya/tryme/MainActivity.java
+++ TryMe/app/src/main/java/com/example/priya/tryme/MainActivity.java
@@ -24,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        colors = new int[]{Color.CYAN, Color.YELLOW,Color.RED, Color.GRAY, Color.GREEN};
-        color_Names = new String[]{"CYAN","YELLOW","RED","GRAY","GREEN"};
+        colors = new int[]{Color.CYAN, Color.YELLOW,Color.RED, Color.GRAY, Color.GREEN, Color.WHITE, Color.BLACK, Color.BLUE, Color.MAGENTA, Color.LTGRAY, Color.rgb(255,67,0),
+                Color.rgb(232,12,111), Color.rgb(4,12,111),Color.rgb(255,148,13),Color.rgb(233,181,111),
+                Color.rgb(47,255,173), Color.rgb(255,124,11), Color.rgb(102,1,110), Color.rgb(48,0, 51),Color.rgb(255,255,0)};
+        color_Names = new String[]{"CYAN","YELLOW","RED","GRAY","GREEN", "WHITE", "BLACK", "BLUE", "MAGENTA", "LIGHT GRAY", "ORANGE", "HOT PINK", "DARK BLUE",
+                "GOLDEN YELLOW", "BEIGE", "MINT GREEN", "LIGHT ORANGE", "PURPLE", "DARK PURPLE", "YELLOW"};
 
 
         super.onCreate(savedInstanceState);
@@ -44,7 +47,14 @@ public class MainActivity extends AppCompatActivity {
                 int randNum = rand.nextInt(colorArrayLength);
                 windowView.setBackgroundColor(colors[randNum]);
                 colorNameView.setVisibility(View.VISIBLE);
+                if(randNum == 6 || randNum == 12 || randNum == 7 || randNum == 18) {
+                    colorNameView.setTextColor(Color.WHITE);
+                } else {
+                    colorNameView.setTextColor(Color.BLACK);
+                }
                 colorNameView.setText(color_Names[randNum]);
+                colorNameView.setFocusable(false);
+                Log.d("Random",String.valueOf(randNum));
             }
         });
     }
